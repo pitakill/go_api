5808b073bd69076a65f80ad3934c5e1958cad5ab
Leopoldo Caballero <git@pitakill.net>
2016-11-14 18:44:00 -0600

    Add Codecov (#12)

--- run-test.sh
+++ run-test.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-find . ! -path '*.git*' ! -path '*backend-config*' ! -path './config' -type d -exec go test {} \;
+find . ! -path '*.git*' ! -path '*backend-config*' ! -path './config' -type d -exec go test -v -covermode=atomic -coverprofile=coverage.txt {} \;
